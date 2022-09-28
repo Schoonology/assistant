e78ee1433e03147d7f3ae483d5f98c5bf14a4437
Michael Schoonmaker <hello@schoonology.com>
2022-09-27 20:40:45 -0400

    Add SQLite to store data.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Particle from "particle-api-js";
 import Wemo from "wemo-client";
+import Database from "better-sqlite3";
 
 const { PARTICLE_USERNAME, PARTICLE_PASSWORD } = process.env;
 
@@ -43,5 +44,40 @@ async function toggleWemo(state: boolean) {
   });
 }
 
+async function setupDb() {
+  const db = new Database(":memory:");
+
+  const createStatement = db.prepare(
+    "CREATE TABLE IF NOT EXISTS data (entity text, key text, value text)"
+  );
+  const createResult = await createStatement.run();
+
+  console.log("Setup:", createResult);
+
+  const initialGetStatement = db.prepare(
+    "SELECT value FROM data WHERE entity = 'a' AND key = 'b'"
+  );
+  const initialGetResult = await initialGetStatement.get();
+
+  console.log("Initial:", initialGetResult);
+
+  const writeStatement = db.prepare(
+    "INSERT INTO data (entity, key, value) VALUES ('a', 'b', ?)"
+  );
+  const writeResult = await writeStatement.run(["42"]);
+
+  console.log("Write:", writeResult);
+
+  const postWriteStatement = db.prepare(
+    "SELECT value FROM data WHERE entity = 'a' AND key = 'b'"
+  );
+  const postWriteResult = await postWriteStatement.get();
+
+  console.log("Post write:", postWriteResult);
+
+  db.close();
+}
+
 // toggleRelayBrick(true);
 // toggleWemo(false);
+setupDb();
